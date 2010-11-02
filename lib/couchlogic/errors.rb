6344f9dc241b0c85f480f90e545fe22675f3bfe5
Mauricio Gomes <mauricio@edge14.com>
2010-11-01 23:11:27 -0400

    Added an error for when a document or database already exists instead of just returning PreExistingConditionFailed

--- lib/couchlogic/errors.rb
+++ lib/couchlogic/errors.rb
@@ -3,6 +3,7 @@ module Couchlogic
   class CouchlogicError         < StandardError; end
   class Unauthorized            < CouchlogicError; end
   class NotFound                < CouchlogicError; end
+  class AlreadyExists           < CouchlogicError; end
   class ServerError             < CouchlogicError; end
   class Unavailable             < CouchlogicError; end
   class DecodeError             < CouchlogicError; end
